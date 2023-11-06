b55d1c695f1e3c8ef4a2c39115b328bb85f93071
Harry Metske <harry.metske@gmail.com>
2023-11-06 11:23:44 +0100

    prepare for boshrelease

--- scripts/start_server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-main() {
-    scripts/setup_dev_grpc_certs.sh
-    echo -e "CA is available at $(pwd)/grpc-kms-certs/grpc_kms_ca_cert.pem"
-    go run main.go -socket /tmp/socket.sock  -public-key-file grpc-kms-certs/grpc_kms_server_cert.pem  -private-key-file grpc-kms-certs/grpc_kms_server_key.pem
-}
-
-main
